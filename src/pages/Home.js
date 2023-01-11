86c114351834b83318d8fa9b092910b9391a2196
AnamariaMantescu <115449098+AnamariaMantescu@users.noreply.github.com>
2023-01-11 17:45:43 +0200

    Add react-render html
    Addin library for render html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,39 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import parse from 'html-react-parser';
 
-// import {addNote, updateNote} from "../redux/actionCreators";
+const Home = () => {
+  const notes = useSelector((state) => state.notes);
+  console.log("notes", notes);
 
-import {Link} from "react-router-dom"
+  return (
+    <div className="App">
+      <h1>React Notes</h1>
+      <div className="container">
+        <Link to="/note">
+          <button type="button" className="btn btn-primary mb-2">
+            Add New Note
+          </button>
+        </Link>
 
-const Home =()=>{
-    const notes = useSelector(state => state.notes);
-    console.log('notes',notes)
-
-
-      return (
-          <div className="App">
-              {/* <Title /> */}
-            <h1>React Notes</h1>
-              <div className="container">
-                     <Link to="/note">
-                        <button type="button" className="btn btn-primary mb-2">Add New Note</button>
-                    </Link>
-
-                {notes.map( i => {
-                    return (
-                    <div className="card mb-2" onClick={() => {}} key={i.id}>
-                        <div className="card-body">
-                            {/* <h5>{i.title}</h5> */}
-                            <h5 className="card-title">{i.title}</h5>
-                            <div className="card-text">{ (i.content) }</div>
-                            <Link to={`/note`} className="card-link">Edit</Link>
-                        </div>
-                    </div>  
-                    )
-                })
-                }
-            
+        {notes.map((i) => {
+          return (
+            <div className="card mb-2" onClick={() => {}} key={i.id}>
+              <div className="card-body">
+                <h5 className="card-title">{i.title}</h5>
+                <div className="card-text">{parse(i.content)}</div>
+                <Link to={`/note`} className="card-link">
+                  Edit
+                </Link>
               </div>
-              
-              {/* <Footer /> */}
-          </div>
-      );
-  }
+            </div>
+          );
+        })}
+      </div>
 
-  
+    </div>
+  );
+};
 
-
-export default Home
+export default Home;

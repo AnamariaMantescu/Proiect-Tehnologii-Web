9cd401e57c9989d1dce7d207366f7e4599f1a863
AnamariaMantescu <115449098+AnamariaMantescu@users.noreply.github.com>
2023-01-12 15:29:26 +0200

    adding color & bg color to text editor

--- client/src/pages/Note.js
+++ client/src/pages/Note.js
@@ -9,8 +9,8 @@ const Note = () => {
   const location = useLocation(); //use the useLocation hook to get props
   const navigate = useNavigate(); //use the useNavigate hook to navigate back to the previous page
   const dispatch = useDispatch(); // use the useDispatch hook to dispatch the addNote action
-  const item = location.state?.item ?? null;//check if item exist
-  const [newNote] = useState(!item ? true : false);// check if it's a new Note
+  const item = location.state?.item ?? null; //check if item exist
+  const [newNote] = useState(!item ? true : false); // check if it's a new Note
   const [content, setContent] = useState(item ? item.content : ""); // initialize the state for the text entered in the editor
   const [title, setTitle] = useState(item ? item.title : ""); //initialize the state for the title of the note
 
@@ -25,11 +25,11 @@ const Note = () => {
       alert("Titlul este obligatoriu");
       return;
     }
-     // Dispatch addNote action with the new note data
+    // Dispatch addNote action with the new note data
     if (newNote) {
       dispatch(addNote({ title, content }));
-    }else{
-      dispatch(editNote({id:item.id,title,content}))
+    } else {
+      dispatch(editNote({ id: item.id, title, content }));
     }
     setTitle("");
     setContent("");
@@ -37,7 +37,7 @@ const Note = () => {
   };
 
   return (
-    <div>
+    <div className="mx-3">
       <h1>Notes</h1>
       <div className="d-flex justify-content-center flex-row mb-2">
         <input
@@ -76,7 +76,92 @@ Note.modules = {
   toolbar: [
     [{ header: "1" }, { header: "2" }, { font: [] }],
     [{ size: [] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      "bold",
+      "italic",
+      "underline",
+      "strike",
+      "blockquote",
+      {
+        color: [
+          "#000000",
+          "#e60000",
+          "#ff9900",
+          "#ffff00",
+          "#008a00",
+          "#0066cc",
+          "#9933ff",
+          "#ffffff",
+          "#facccc",
+          "#ffebcc",
+          "#ffffcc",
+          "#cce8cc",
+          "#cce0f5",
+          "#ebd6ff",
+          "#bbbbbb",
+          "#f06666",
+          "#ffc266",
+          "#ffff66",
+          "#66b966",
+          "#66a3e0",
+          "#c285ff",
+          "#888888",
+          "#a10000",
+          "#b26b00",
+          "#b2b200",
+          "#006100",
+          "#0047b2",
+          "#6b24b2",
+          "#444444",
+          "#5c0000",
+          "#663d00",
+          "#666600",
+          "#003700",
+          "#002966",
+          "#3d1466",
+          "custom-color",
+        ],
+      },
+      {background: [
+        "#000000",
+        "#e60000",
+        "#ff9900",
+        "#ffff00",
+        "#008a00",
+        "#0066cc",
+        "#9933ff",
+        "#ffffff",
+        "#facccc",
+        "#ffebcc",
+        "#ffffcc",
+        "#cce8cc",
+        "#cce0f5",
+        "#ebd6ff",
+        "#bbbbbb",
+        "#f06666",
+        "#ffc266",
+        "#ffff66",
+        "#66b966",
+        "#66a3e0",
+        "#c285ff",
+        "#888888",
+        "#a10000",
+        "#b26b00",
+        "#b2b200",
+        "#006100",
+        "#0047b2",
+        "#6b24b2",
+        "#444444",
+        "#5c0000",
+        "#663d00",
+        "#666600",
+        "#003700",
+        "#002966",
+        "#3d1466",
+        "custom-color",
+      ],}
+    ],
+
     [
       { list: "ordered" },
       { list: "bullet" },
@@ -105,7 +190,8 @@ Note.formats = [
   "indent",
   "link",
   "image",
-  "video",
+  "color",
+  "background",
 ];
 
 export default Note;
